eb824f7910a771f8e56bee3e6881959ed841f75a
Okarin1 <Okarin1024@gmail.com>
2021-10-25 19:49:53 +0800

    导航栏的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,25 @@
 <template>
-<h1>Home</h1>
+  <div>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
 export default {
-  name: "Home"
+  name: "Home",
+  components:{
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-tint);
+  color: white;
+  font-size: large;
+  font-weight: bold;
+}
 </style>
