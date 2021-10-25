0a392f92033472373f838a5da4b5662c3b61c7c3
Okarin1 <Okarin1024@gmail.com>
2021-10-25 21:44:14 +0800

    首页推荐的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners = "banners"/>
-<!--    <li v-for="i in banners">{{i.title}}</li>-->
-<!--    <li v-for="i in recommends">{{i.title}}</li>-->
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends" />
+
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
 
 
 import {getHomeMultidata} from "@/network/home";
 
 
+
 export default {
   name: "Home",
   components:{
+    RecommendView,
     HomeSwiper,
     NavBar,
   },
