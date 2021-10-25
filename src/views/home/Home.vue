2d8cf13ed6a4d686f0d2984715e6d8505c3d1169
Okarin1 <Okarin1024@gmail.com>
2021-10-25 20:49:43 +0800

    网络请求的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <h1>Home</h1>
+    <h1>home</h1>
+    <li v-for="i in banners">{{i.title}}</li>
+    <li v-for="i in recommends">{{i.title}}</li>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
 export default {
   name: "Home",
   components:{
     NavBar
+  },
+  data(){
+    return{
+       banners:[],
+      recommends:[]
+    }
+  },
+  created() {
+    getHomeMultidata().then(res =>{
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
   }
 }
 </script>
