924a109e40e15499cd4b0156ab487ed6e3685bec
Okarin1 <Okarin1024@gmail.com>
2021-10-27 18:37:21 +0800

    首页推荐位的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends" />
+    <feature-view/>
 
   </div>
 </template>
@@ -11,7 +12,7 @@
 import NavBar from "@/components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
-
+import FeatureView from "@/views/home/childComps/FeatureView";
 
 import {getHomeMultidata} from "@/network/home";
 
@@ -23,6 +24,7 @@ export default {
     RecommendView,
     HomeSwiper,
     NavBar,
+    FeatureView
   },
   data(){
     return{
@@ -40,11 +42,19 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: white;
   font-size: large;
   font-weight: bold;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 
 
